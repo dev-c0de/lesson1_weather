caf1c3e8f16000c274cda92e894fb4534f9eaebc
Valeriy S <smirnov_va@outlook.com>
2023-04-12 12:28:48 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
 import requests
 
-places = ['Лондон', 'Шереметьево', 'Череповц']
-url = 'https://wttr.in/'
-payload = {"lang": "ru"}
-for place in places:
-    response = requests.get(url+place+'?nTqm', params=payload)
-    response.raise_for_status()
-    print(response.text)
+def get_weather(places):
+    url = 'https://wttr.in/'
+    payload = {"n":"","T":"","q":"", "M":"", "lang": "ru"}
+    for place in places:
+        response = requests.get(f'{url}{place}', params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+if __name__ == '__main__':
+    get_weather(['Лондон', 'Шереметьево', 'Череповец'])
