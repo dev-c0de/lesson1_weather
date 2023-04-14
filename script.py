0b0e243e7dea83a93897cf246103517c04e53feb
Valeriy S <smirnov_va@outlook.com>
2023-04-14 08:08:40 +0300

    update script.py  (flake8)

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 import requests
 
+
 def get_weather(places):
     url = 'https://wttr.in/'
-    payload = {"n":"","T":"","q":"", "M":"", "lang": "ru"}
+    payload = {"n": "", "T": "", "q": "", "M": "", "lang": "ru"}
     for place in places:
         response = requests.get(f'{url}{place}', params=payload)
         response.raise_for_status()
         print(response.text)
+
 
 if __name__ == '__main__':
     get_weather(['Лондон', 'Шереметьево', 'Череповец'])
